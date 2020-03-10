6f657467b45a913fee2865f17b9339baa07fb9ed
Guillermo de la Iglesia <zguillez@gmail.com>
2020-03-10 01:14:42 +0100

    update

--- example/index.php
+++ example/index.php
@@ -2,6 +2,5 @@
 require 'vendor/autoload.php';
 //----------------------------
 $db = new Z\Database('***.***.***.***', 'user', '******', 'database');
-/** */
 $data = $db->sql2array('SELECT * FROM users');
 $db->test($data);
